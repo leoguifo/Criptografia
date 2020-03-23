802295a75766757ec7e286f0ea0e975c68e96947
Lucien Risso Correia <lucien@multiplier.com.br>
2020-03-22 23:18:29 -0300

    implementado funções no html

--- src/crypt.js
+++ src/crypt.js
@@ -4,8 +4,12 @@ const crypto = require('crypto');
 const alg = 'aes-256-ctr';
 const passwd = 'abcdabcd';
 
-const read = fs.createReadStream('input.txt');
-const write = fs.createWriteStream('output.crypt');
-const cipher = crypto.createCipher(alg, passwd);
-
-read.pipe(cipher).pipe(write);
+module.exports = crypt = (file) => {
+    fs.appendFile('input.txt', file, () => {
+        const read = fs.createReadStream('input.txt');
+        const write = fs.createWriteStream('output.crypt');
+        const cipher = crypto.createCipher(alg, passwd);
+    
+        read.pipe(cipher).pipe(write);
+    });
+}
--- src/server.js
+++ src/server.js
@@ -1,21 +1,51 @@
 const http = require('http');
 const fs = require('fs');
-
-// Create an HTTP server
+const crypt = require('./crypt');
+const decrypt = require('./decrypt');
 
 fs.readFile('./index.html', (err, html) => {
     const srv = http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(html);
-        res.end();
 
-        console.log(req.on());
+        if (req.method == 'POST') {
+            if (req.url == '/encrypt') {
+                var body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                req.on('end', () => {
+                    crypt(body);
+                    fs.readFile('./output.crypt', (err, file) => {
+                        res.setHeader('Content-Disposition', ' attachment; filename="encrypt.crypt"');
+                        res.write(file);
+                        res.end();
+                    });
+
+                });
+            }
+
+            if (req.url == '/decrypt') {
+                var body = '';
+                req.on('data', (data) => {
+                    body += data;
+                });
+                req.on('end', () => {
+                    decrypt(body);
+                    fs.readFile('./decrypted.txt', (err, file) => {
+                        res.setHeader('Content-Disposition', ' attachment; filename="decrypt.txt"');
+                        res.write(file);
+                        res.end();
+                    });
+                });
+            }
+
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(html);
+            res.end();
+        }
     });
 
-    
-    
     // Now that server is running
     srv.listen(8000, '127.0.0.1', () => {
-    
     });
 });
